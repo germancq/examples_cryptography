86f3165f55bd0ea87f602bfb5c6775103327547f
germancq <germancq@gmail.com>
2018-05-17 12:32:57 +0200

    trivium.py input and output formated in hexadecimal values

--- python/trivium.py
+++ python/trivium.py
@@ -23,37 +23,36 @@
 #generates up to 2^64 bits of keystream
 
 from collections import deque
+from bitstring import BitArray
 import re
 
 def str_to_bin(string):
     s = ' '.join(format(x, '08b') for x in bytearray(string,'utf-8'))
-    print(s)
     s = re.sub(r'\s+','',s)
     return s
 
 class Trivium:
-    A = deque([format(0,'b')]*93)
-    B = deque([format(0,'b')]*84)
-    C = deque([format(0,'b')]*112)
+    A = deque([0]*93)
+    B = deque([0]*84)
+    C = deque([0]*111)
 
 
     def Initialization(self,key,IV):
-        key_bin = str_to_bin(key)
-        IV_bin = str_to_bin(IV)
+
+        #hex to bits
+        key_ = BitArray(hex=key)
+        IV_ = BitArray(hex=IV)
 
 
 
-        if self.check_len(key_bin,IV_bin) == 0:
-            return 0
+        self.check_len(key_.bin,IV_.bin)
 
-        for i in range(0,len(key_bin)):
-            self.B[i] = key_bin[i]
 
-        for j in range(0,len(IV_bin)):
-            self.A[j] = IV_bin[j]
+        for i in range(0,len(key_.bin)):
+            self.B[i] = int(key_.bin[i])
 
-        print(self.A)
-        print(self.B)
+        for j in range(0,len(IV_.bin)):
+            self.A[j] = int(IV_.bin[j])
 
         return 1
 
@@ -62,55 +61,47 @@ class Trivium:
             self.step()
 
 
-    def Encryption(self,length,sol_register):
+    def Encryption(self,length):
         for i in range(0,length):
-            sol_register[i] = self.step()
+            yield self.step()
 
     def step(self):
-        sa = self.xor(self.A[92],self.A[65])
-        sb = self.xor(self.B[83],self.B[68])
-        sc = self.xor(self.C[110],self.C[65])
-        s = self.xor(sa,self.xor(sb,sc))
+        sa = self.A[92] ^ self.A[65]
+        sb = self.B[83] ^ self.B[68]
+        sc = self.C[110] ^ self.C[65]
+        s = sa ^ sb ^ sc
         a00 = self.A[68]
-        a01 = self.and_(self.C[108],self.C[109])
-        a0 = self.xor(sc,self.xor(a00,a01))
+        a01 = self.C[108] & self.C[109]
+        a0 = sc ^ a00 ^ a01
         b00 = self.B[77]
-        b01 = self.and_(self.A[90],self.A[91])
-        b0 = self.xor(sa,self.xor(b00,b01))
+        b01 = self.A[90] & self.A[91]
+        b0 = sa ^ b00 ^ b01
         c00 = self.C[86]
-        c01 = self.and_(self.B[81],self.B[82])
-        c0 = self.xor(sb,self.xor(c00,c01))
-        self.shift_left(self.A,a0)
-        self.shift_left(self.B,b0)
-        self.shift_left(self.C,c0)
+        c01 = self.B[81] & self.B[82]
+        c0 = sb ^ c00 ^ c01
+        self.shift_right(self.A,a0)
+        self.shift_right(self.B,b0)
+        self.shift_right(self.C,c0)
         return s
 
-    def shift_left(self,register,value):
+    def shift_right(self,register,value):
         register.rotate(1)
-        register[0] = value
+        register[0] = int(value)
 
 
     def check_len(self,key,IV):
         if len(key) > 80 or len(IV) > 80:
-            return 0
-        return 1
+            raise ValueError('values for key an IV must not exceed 80 bits')
 
-    def xor(self,str1,str2):
-        if str1 == str2:
-            return '0'
-        else:
-            return '1'
 
-    def and_(self,str1,str2):
-        if str1 != '0' and str2 != '0':
-            return '1'
-        else:
-            return '0'
 
 if __name__ == "__main__":
+    key = "01020304050607080900"#HEX
+    IV = "01020304050607080900"#HEX
+
+
     trivium = Trivium()
-    s = [0]*80
-    trivium.Initialization('1234567890','1234567890')
+    trivium.Initialization(key,IV)
     trivium.Warm_up()
-    trivium.Encryption(80,s)
-    print(s)
+    s = trivium.Encryption(80)
+    print (BitArray(s).hex)
