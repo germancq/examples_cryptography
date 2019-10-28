c2f76f53488d34cb7b35563a71cacadddbe8e8e2
germancq <germancq@gmail.com>
2019-10-28 18:31:21 +0100

    still develop twofish.py

--- python/twofish.py
+++ python/twofish.py
@@ -6,10 +6,12 @@
 #    By: germancq <germancq@dte.us.es>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/10/21 16:43:56 by germancq          #+#    #+#              #
-#    Updated: 2019/10/23 18:08:09 by germancq         ###   ########.fr        #
+#    Updated: 2019/10/28 18:29:40 by germancq         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+import math
+import struct
 '''
 key 128 bits
 '''
@@ -44,56 +46,78 @@ class Twofish :
         self.S_i = k[2]
 
     def encrypt(self,plaintext) :    
-        plaintext_0 = plaintext & (2**32 - 1)
-        plaintext_1 = (plaintext>>32) & (2**32 - 1)
-        plaintext_2 = (plaintext>>64) & (2**32 - 1)
-        plaintext_3 = (plaintext>>96) & (2**32 - 1)
+        plaintext_3 = swap32(plaintext & (2**32 - 1))
+        plaintext_2 = swap32((plaintext>>32) & (2**32 - 1))
+        plaintext_1 = swap32((plaintext>>64) & (2**32 - 1))
+        plaintext_0 = swap32((plaintext>>96) & (2**32 - 1))
         
+        print(hex(plaintext_0))
+        print(hex(plaintext_1))
+        print(hex(plaintext_2))
+        print(hex(plaintext_3))
+
         k_0 = generate_K_values(0,self.M_e,self.M_o)
-        print(hex(k_0[0]))
-        print(hex(k_0[1]))
+        #print(hex(k_0[0]))
+        #print(hex(k_0[1]))
         
         k_1 = generate_K_values(1,self.M_e,self.M_o)
 
-        print(hex(k_1[0]))
-        print(hex(k_1[1]))
-        '''
+        #print(hex(k_1[0]))
+        #print(hex(k_1[1]))
+        
         R_0 = plaintext_0 ^ k_0[0]
         R_1 = plaintext_1 ^ k_0[1]
         R_2 = plaintext_2 ^ k_1[0]
         R_3 = plaintext_3 ^ k_1[1]
 
         
-        for i in range(0,15):
-            #print(i)
-            #print(hex(R_0))
-            #print(hex(R_1))
-            #print(hex(R_2))
-            #print(hex(R_3))
+        for i in range(0,16):
+            print(i)
+            print("R_values")
+            print(hex(R_0))
+            print(hex(R_1))
+            print(hex(R_2))
+            print(hex(R_3))
+            aux_0 = R_0
+            aux_1 = R_1
             f = function_F(R_0,R_1,i,self.M_e,self.M_o,self.S_i)
+            
             R_0 = ROR(f[0] ^ R_2, 1, 32)      
             R_1 = ROL(R_3,1,32) ^ f[1]  
-            R_2 = R_0
-            R_3 = R_1
+            R_2 = aux_0
+            R_3 = aux_1
+            
 
         k_2 = generate_K_values(2,self.M_e,self.M_o)   
-        k_3 = generate_K_values(3,self.M_e,self.M_o)     
+        #print(hex(k_2[0]))
+        #print(hex(k_2[1]))
+        k_3 = generate_K_values(3,self.M_e,self.M_o)  
+        #print(hex(k_3[0]))
+        #print(hex(k_3[1]))   
         
         C_0 = R_2 ^ k_2[0]
         C_1 = R_3 ^ k_2[1]
         C_2 = R_0 ^ k_3[0]
         C_3 = R_1 ^ k_3[1]
+        print(hex(C_0))
+        print(hex(C_1))
+        print(hex(C_2))
+        print(hex(C_3))
 
         return (C_3 << 96) + (C_2 << 64) + (C_1 << 32) + C_0 
-        '''
-        return 0
-
+        
+        
 def function_F(x,y,round,M_e,M_o,S_i):
 
     T0 = function_g(x,S_i)
     y = ROL(y,8,32)
     T1 = function_g(y,S_i)
-    K_r = generate_K_values(round+8,M_e,M_o)
+    K_r = generate_K_values(round+4,M_e,M_o)
+    print("T_values")
+    #print(round)
+    
+    print(hex(T0))
+    print(hex(T1))
     F0 = (T0 + T1 + K_r[0]) & (2**32 - 1)
     F1 = (T0 + (T1<<1) + K_r[1]) & (2**32 - 1)
     return (F0,F1)
@@ -112,7 +136,7 @@ def key_schedule(key):
     for i in range(0,2):
         for j in range(0,8):
             m_i[i].append((key>>(8*(i+j))) & 0xFF)
-        r = matrix_multiplication(RS,m_i[i])
+        r = matrix_multiplication_GF256(RS,m_i[i],0x169)
         
         S_i.append((r[3][0]<<24) + (r[2][0]<<16) + (r[1][0]<<8) + r[0][0])
             
@@ -125,6 +149,12 @@ def key_schedule(key):
     
     return (M_e,M_o,S_i) 
 
+
+def swap32(x):
+    return (((x << 24) & 0xFF000000) |
+            ((x <<  8) & 0x00FF0000) |
+            ((x >>  8) & 0x0000FF00) |
+            ((x >> 24) & 0x000000FF))
 
 def function_h(X,L):
     #print("start function_h")
@@ -143,6 +173,7 @@ def function_h(X,L):
         x_2 = (input_x>>16) & 0xFF
         x_3 = (input_x>>24) & 0xFF
         
+        
         y_0 = generate_q_output(selected_boxes[i][0],x_0)
         y_1 = generate_q_output(selected_boxes[i][1],x_1)
         y_2 = generate_q_output(selected_boxes[i][2],x_2)
@@ -158,13 +189,13 @@ def function_h(X,L):
         #y = y & (2**32 - 1)
     
 
-    print(hex(y))
-    z_matrix =  matrix_multiplication(MDS,[y_0,y_1,y_2,y_3]) 
+    #print(hex(y))
+    z_matrix =  matrix_multiplication_GF256(MDS,[y_0,y_1,y_2,y_3],0x169) 
     
-    print(hex(z_matrix[3][0]))
-    print(hex(z_matrix[2][0]))
-    print(hex(z_matrix[1][0]))
-    print(hex(z_matrix[0][0]))
+    #print(hex(z_matrix[3][0]))
+    #print(hex(z_matrix[2][0]))
+    #print(hex(z_matrix[1][0]))
+    #print(hex(z_matrix[0][0]))
 
     z_3 = z_matrix[3][0] & 0xFF
     z_2 = z_matrix[2][0] & 0xFF
@@ -207,18 +238,16 @@ def generate_q_output(q_i,x):
 
 def generate_K_values(i,M_e,M_o):
     p = 2**24 + 2**16 + 2**8 + 2**0
-    A = function_h(p<<i,M_e)
-    print(hex(A))
-    B = function_h((p<<i)+1,M_o)
+    A = function_h(2*i*p,M_e)
+    B = function_h((2*i+1)*p,M_o)
     B = ROL(B,8,32)
-    print(hex(B))
-    K_0 = A+B & (2**32 - 1) #K_2i
-    K_1 = (A+(B<<1)) & (2**32 - 1)
+    K_0 = (A+B) % (2**32) #K_2i
+    K_1 = (A+(B<<1)) % (2**32)
     K_1 = ROL(K_1,9,32) #K_2i+1
     return (K_0,K_1)
 
 
-def matrix_multiplication(M1,M2) :
+def matrix_multiplication_GF256(M1,M2,p) :
     
     rows_a = len(M1)
     rows_b = len(M2) # == colums_a
@@ -240,16 +269,66 @@ def matrix_multiplication(M1,M2) :
                 if(colums_b > 1):
                     a_3 = M2[k][j]
                 else :
-                    a_3 = M2[j]
+                    a_3 = M2[k]
 
                 if(colums_a > 1) :
                     a_2 = M1[i][k]   
                 else :
                      a_2 = M1[k]
                     
-                result[i][j] = (a_1 + (a_2 * a_3))
+                #result[i][j] = (a_1 + (a_2 * a_3))
+                gf_1 = galois_multiplication(a_2,a_3,p)
+                '''
+                print("==========================")
+                print(i)
+                print(j)
+                print(hex(a_2))
+                print(hex(a_3))
+                print(hex(gf_1))
+                print(hex(a_1))
+                print("==========================")
+                '''
+                result[i][j] = galois_add(a_1,gf_1)
 
     return result
+
+
+def galois_add(a,b) :
+    return a ^ b    
+
+def galois_multiplication(a,b,p):    
+    #GF(256)
+    t = 0
+    for i in range(0,8):
+        mask = 0x1<<i
+        bit_b = b & mask
+        b_i = 0 if bit_b == 0 else 1 
+        r = b_i*(a<<i) 
+        t = galois_add(t,r)
+        
+
+    t = reduced_polinomial_GF256(t,p)
+       
+
+
+    return t    
+
+def reduced_polinomial_GF256(a,p):
+    #grado del polinomio p
+    n = math.ceil(math.log(p,2))
+    n = n - 1 
+    t = a
+    for i in range(15,7,-1):
+        mask = 0x1<<i
+        bit_t = t & mask
+        t_i = 0 if bit_t == 0 else 1
+        if(t_i):
+            m = int(i - n)
+            t = galois_add(p<<m ,t)
+
+    return t    
+
+
 
 
 def ROL(x,i,len_bits):
@@ -261,11 +340,22 @@ def ROR(x,i,len_bits):
     return ((x>>i) | (x<<j)) & (2**len_bits - 1)     
 
 
+
 if __name__ == "__main__":
     
     key = 0x0
-    plaintext = 0x0
+    plaintext = 0xFFFFFFFFFFFFFFFFFFFFFFFF00000001
     cipher = Twofish(key)
     result = cipher.encrypt(plaintext)
-    print(hex(result))
     
+    #result = galois_multiplication(0x5b,0xfc,0x169)
+    #result = matrix_multiplication_GF256(MDS,[0x98,0xCA,0xe0,0xFC],0x169)
+    
+    print(hex(result))
+    z = 0
+    for i in range(0,16):
+        x = ((result >> 8*i & 0xFF)<< 8*(15-i))
+        z = z | x
+
+    print(hex(z))    
+    #print(result)
